84fc12a07e6caf1e44dc723e396b67ef862c1e2b
Evgeniy Dmitrenko <ewgeniy.dmitrenko@gmail.com>
2021-05-20 21:12:09 +0300

    add main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 5000;
+        int bonus = 20;
+        int bonusBalance = ticketPrice / bonus;
+        System.out.println(bonusBalance);
+    }
+}
